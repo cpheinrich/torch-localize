5cce0f0f733a1750bb59d76cfa7338072eff072d
Chris Heinrich <cpheinrich@gmail.com>
2019-08-13 08:12:13 -0700

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='torch-localization',
+    version='0.0.1',
+    description='Dimensionality annotations for tensor parameters and return values',
+    packages=['torch_localization'],
+    author='Michał Tyszkiewicz',
+    author_email='michal.tyszkiewicz@gmail.com',
+)
