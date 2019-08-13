1275d7b9ac9640630a3d45507603c6afb3fc41cc
Chris Heinrich <cpheinrich@gmail.com>
2019-08-13 08:19:11 -0700

    updated package structure

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='torch-localization',
+    name='torch-locale',
     version='0.0.1',
     description='Dimensionality annotations for tensor parameters and return values',
-    packages=['torch_localization'],
+    packages=['torch_localize'],
     author='Michał Tyszkiewicz',
     author_email='michal.tyszkiewicz@gmail.com',
 )
